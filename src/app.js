5e4747b12da376c78b4dd1a9ebe8c32b577d0be3
Derek <derekluu90@gmail.com>
2019-10-23 21:18:51 -0400

    remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -16,22 +16,6 @@ import 'react-dates/lib/css/_datepicker.css';
 const store = configureStore();
 //configureStore returns an object with store methods. it does not hold return the 'state' - to do that you must used the method getStat below
 
-store.dispatch(addExpense({ 
-    description: 'Water bill',
-    amount: 4500
-}));
-store.dispatch(addExpense({
-    description: 'Gas bill',
-    createdAt: 1000
-}));
-store.dispatch(addExpense({
-    description: 'Rent',
-    amount: 109500
-}));
-
-const state = store.getState();
-const expenses = getVisibleExpenses( state.expenses, state.filters );
-
 
 const jsx = (
     // provider - provides the store object to all the child components. enables the use of connect, connecting the components to the store
