e1a674891ded74d5adfa7bd4682ece63a004b68f
Derek <derekluu90@gmail.com>
2019-11-03 20:21:55 -0500

    working webpack after mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,35 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    
-    }, {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true //return index.html for 404 errors
-  }
-};
+module.exports = (env) => {
+  const isProduction = env === 'production'// returns boolean value
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+
+      }, {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }]
+    },
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map', //inline version will take longer to run build, but smaller. suitable for producion purpose
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true //return index.html for 404 errors
+    }
+  };
+
+}
+
 
