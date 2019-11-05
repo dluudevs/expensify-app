063fe014e7b5cf57f191b456d0b1e39847b97fe5
Derek <derekluu90@gmail.com>
2019-11-04 21:04:48 -0500

    setup devdependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,10 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = (env) => {
   const isProduction = env === 'production'// returns boolean value
-  console.log(env)
   return {
     entry: './src/app.js',
     output: {
-      path: path.join(__dirname, 'public'), //currently directory name + /public
+      path: path.join(__dirname, 'public','dist'), //currently directory name + /public +/dist
       filename: 'bundle.js'
     },
     module: {
@@ -43,7 +42,8 @@ module.exports = (env) => {
     devtool: isProduction ? 'source-map' : 'inline-source-map', //inline version will take longer to run build, but smaller. suitable for producion purpose
     devServer: {
       contentBase: path.join(__dirname, 'public'),
-      historyApiFallback: true //return index.html for 404 errors
+      historyApiFallback: true, //return index.html for 404 errors
+      publicPath: '/dist/' //so the server knows where to retrieve the assets, as they are longer sitting in the public folder (public/dist)
     }
   };
 
